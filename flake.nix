9f848b9a498523bcc0bb0b1905347d157b55f435
Idris Raja <idris.raja@gmail.com>
2024-08-01 11:03:29 -0500

    get things working

--- flake.nix
+++ flake.nix
@@ -1,13 +1,26 @@
 {
   description = "template for math proofs";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/23.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.hippoid-tex.url = "github:idrisr/hippoid-tex";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    hippoid-tex.url = "github:idrisr/hippoid-tex";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    devenv.url = "github:cachix/devenv";
+  };
 
-  outputs = { nixpkgs, flake-utils, hippoid-tex, ... }:
+  outputs = inputs@{ nixpkgs, flake-utils, hippoid-tex, ... }:
     let
       system = flake-utils.lib.system.x86_64-linux;
-      pkgs = import nixpkgs { inherit system; };
+      hooks = {
+        nixfmt.enable = true;
+        deadnix.enable = true;
+        beautysh.enable = true;
+        lacheck.enable = true;
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ hippoid-tex.overlays.hippoid-tex ];
+      };
       cleaner = pkgs.writeShellApplication {
         runtimeInputs = [ tex ];
         name = "clean";
@@ -21,20 +34,33 @@
         name = "makepdf";
         text = ''
           mkdir -p aux pdf
-          latexmk -interaction=nonstopmode -lualatex -pdf -auxdir=aux -outdir=pdf ./*tex
+          latexmk -interaction=nonstopmode -lualatex -auxdir=aux -outdir=pdf ./*tex
         '';
       };
       tex = pkgs.texlive.combine {
         inherit (pkgs.texlive) scheme-full;
-        hippoid-tex = {
-          pkgs = [ hippoid-tex.packages.${system}.hippoid-tex ];
-        };
+        hippoid-tex = { pkgs = [ pkgs.hippoid-tex ]; };
       };
     in {
-      packages.${system}.default = tex;
-      devShells.${system} = {
-        default = pkgs.mkShell { buildInputs = [ tex ]; };
+      checks.${system} = {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          inherit hooks;
+        };
       };
+
+      packages.${system}.default = tex;
+      devShells.${system} = let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = inputs.devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [{
+            pre-commit.hooks = hooks;
+            packages = [ tex pkgs.codespell pkgs.python312Packages.pygments ];
+          }];
+        };
+      };
+
       apps.${system} = {
         clean = {
           type = "app";
